8be3792ef216ed87e4a78ab251a597b8192fd2f8
Adailton Junior <aJlino82>
2022-04-08 22:54:00 -0300

    criando operação de instanciação mocking

--- src/main/java/com/crud/app/AulaJavApplication.java
+++ src/main/java/com/crud/app/AulaJavApplication.java
@@ -1,13 +1,32 @@
 package com.crud.app;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.crud.app.domain.Categoria;
+import com.crud.app.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class AulaJavApplication {
+public class AulaJavApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaJavApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
