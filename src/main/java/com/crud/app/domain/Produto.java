f78c1a39b2ce792205ab645847c24fe66ad8aa0a
Adailton Junior <aJlino82>
2022-04-24 23:28:19 -0300

    Modificando a classe produto

--- src/main/java/com/crud/app/domain/Produto.java
+++ src/main/java/com/crud/app/domain/Produto.java
@@ -24,8 +24,10 @@ public class Produto implements Serializable {
 	private Double preco;
 
 	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"),
-			inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
 
 	private List<Categoria> categorias = new ArrayList<>();
 
