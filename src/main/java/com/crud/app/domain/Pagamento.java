c35574c31887fcb5baabed3eb4a2081dddabbf3e
Adailton Junior <53987087+aJlino82@users.noreply.github.com>
2022-05-11 09:55:58 -0300

    implementado classes de dominios

--- src/main/java/com/crud/app/domain/Pagamento.java
+++ src/main/java/com/crud/app/domain/Pagamento.java
@@ -3,14 +3,27 @@ package com.crud.app.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.crud.app.domain.enums.EstadoPagamento;
 
+@Entity
+@Inheritance
 public class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private Long id;
 	private EstadoPagamento estado;
 
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 
 	public Pagamento() {
