5571ea43f270e4fe44566fbb8a5f17da1c5e84a7
Adailton Junior <53987087+aJlino82@users.noreply.github.com>
2022-06-17 16:59:33 -0300

    Listando categorias com DTO

--- src/main/java/com/crud/app/resources/CategoriaResource.java
+++ src/main/java/com/crud/app/resources/CategoriaResource.java
@@ -1,7 +1,7 @@
 package com.crud.app.resources;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.app.domain.Categoria;
+import com.crud.app.dto.CategoriaDTO;
 import com.crud.app.services.CategoriaService;
 
 @RestController
@@ -25,15 +25,18 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService categoriaService;
 
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> catList = categoriaService.findAll();
+		List<CategoriaDTO> catListDTO = catList.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(catListDTO);
+
+	}
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable Long id) {
 		Categoria obj = this.categoriaService.findById(id);
 		return ResponseEntity.ok().body(obj);
-	}
-
-	@GetMapping
-	public List<Categoria> findAll() {
-		return categoriaService.findAll();
 	}
 
 	@PostMapping
