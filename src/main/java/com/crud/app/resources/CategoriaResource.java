aa1d577ef1ef44bf92425808b9e448b11cec6e39
Adailton Junior <53987087+aJlino82@users.noreply.github.com>
2022-07-28 10:51:27 -0300

    Paginacão comparametros na requisicao

--- src/main/java/com/crud/app/resources/CategoriaResource.java
+++ src/main/java/com/crud/app/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.app.domain.Categoria;
@@ -55,6 +57,16 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> deletar(@PathVariable long id) {
 		categoriaService.deleteById(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> catList = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> catListDTO = catList.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(catListDTO);
 	}
 
 }
